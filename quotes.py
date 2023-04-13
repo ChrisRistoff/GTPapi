14540f38e0388ce5ef82306a23208c04d7071df0
krasenHristov <doanusho@gmail.com>
2023-04-13 16:38:02 +0100

    random quote gpt

---
+++ quotes.py
@@ -0,0 +1,39 @@
+import openai
+import configparser
+from collections import deque
+
+system_message = """
+"""
+
+
+def main():
+    prompt = "Give me a random inspirational quote. Do not put it in quotes but put a % symbol between the quote and the author."
+    content = ask_gpt(prompt)
+
+    content = content.split("%")
+    quote = content[0]
+    author = content[1]
+
+    print('"' + quote + '"')
+    print("-" + author)
+
+def ask_gpt(prompt: str):
+    config = configparser.ConfigParser()
+    config.read(".apikey")
+    GPT_API = config.get("API", "GPT_API")
+
+    user_prompt = {"role": "user", "content": prompt}
+
+    openai.api_key = GPT_API
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        user_prompt
+    ]
+    )
+
+    content = response["choices"][0]["message"]["content"]
+    return content
+
+if __name__ == '__main__':
+    main()
